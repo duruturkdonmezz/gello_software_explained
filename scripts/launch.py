840e05e14330ac857f1068f05ad5772fc1c25778
Duru Turkdonmez <103323403+duruturkdonmezz@users.noreply.github.com>
2025-05-26 14:39:08 -0400

    Update launch.py

--- scripts/launch.py
+++ scripts/launch.py
@@ -1,38 +1,39 @@
-import os
-import subprocess
+import os #functionalities for interacting with the OS, such as environment variables and file paths
+import subprocess #enables execution of external commands or programs directly from python
 
-current_file_path = os.path.abspath(__file__)
+current_file_path = os.path.abspath(__file__) #retrieve absolute path of current python script
 
 
 def run_docker_container():
-    user = os.getenv("USER")
-    container_name = f"gello_{user}"
-    gello_path = os.path.abspath(os.path.join(current_file_path, "../../"))
-    volume_mapping = f"{gello_path}:/gello"
+    user = os.getenv("USER") #fetch username
+    container_name = f"gello_{user}" #define container as gello_username
+    gello_path = os.path.abspath(os.path.join(current_file_path, "../../")) #determine absolute path of gello, two levels above current script
+    volume_mapping = f"{gello_path}:/gello" #prepare docker volume mapping from local gello directory to /gello inside container. Ensures changes inside the container affect the original source code
 
+#docker run command construction
     cmd = [
         "docker",
         "run",
-        "--runtime=nvidia",
-        "--rm",
-        "--name",
+        "--runtime=nvidia", #use NVIDIA GPU runtime support
+        "--rm", #autoremove container when it stops running
+        "--name", 
         container_name,
-        "--privileged",
-        "--volume",
+        "--privileged", #grants the container priviliges such as accessing hardware
+        "--volume", 
         volume_mapping,
         "--volume",
-        "/home/gello:/homefolder",
-        "--net=host",
+        "/home/gello:/homefolder", #maps host directory /home/gello to /homefolder insode the container
+        "--net=host", #uses host network stack within the container for simplicity and direct access
         "--volume",
-        "/dev/serial/by-id/:/dev/serial/by-id/",
-        "-it",
-        "gello:latest",
+        "/dev/serial/by-id/:/dev/serial/by-id/", #maps serial devices such as Dynamixel motors
+        "-it", #interactive mode with terminal support
+        "gello:latest", #docker image
         "bash",
-        "-c",
-        "pip install -e third_party/DynamixelSDK/python && exec bash",
+        "-c", #execute command inside bash
+        "pip install -e third_party/DynamixelSDK/python && exec bash", #install DynamixelSDK python package, then runs a new bash shell
     ]
 
-    subprocess.run(cmd)
+    subprocess.run(cmd) #execute assembled docker command
 
 
 if __name__ == "__main__":
